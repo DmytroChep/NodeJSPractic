d7982e5a5a926596931a9f9ceb95cc0085366911
dimachep1408 <dmitriychep2011@gmail.com>
2025-09-17 07:58:29 +0300

    created server on express and page timastamp which return date in json format

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
 const moment = require("moment")
+const express = require("express")
 
-const date = moment()
+// const date = moment()
 
-function getCurrentDay(){
-    console.log(date.format("dddd"))
+// function getCurrentDay(){
+//     console.log(date.format("dddd"))
+// }
+
+// function getCurrentMonth(){
+//     console.log(date.format("MMMM"))
+// }
+
+// function getCurrentYear(){
+//     console.log(date.format("YYYY"))
+// }
+
+
+// getCurrentDay()
+// getCurrentMonth()
+// getCurrentYear()
+
+// //2
+
+// function getDate(){
+//     console.log(date.format("L LTS"))
+// }
+
+// getDate()
+
+
+// function getWeekDay(){
+//     console.log("date geted with split:\n",date.format("LLLL").split(",")[0])
+// }
+
+// getWeekDay()    
+
+function getFullDate(){
+    return moment().format("LLLL")
 }
 
-function getCurrentMonth(){
-    console.log(date.format("MMMM"))
-}
 
-function getCurrentYear(){
-    console.log(date.format("YYYY"))
-}
+const HOST = "127.0.0.1"
+const PORT = 8000
+
+const app = express()
 
 
-getCurrentDay()
-getCurrentMonth()
-getCurrentYear()
+app.get("/timestamp", (req, res) => {
+    res.status(418).json({date: getFullDate()})
+})
 
-//2
-
-function getDate(){
-    console.log(date.format("L LTS"))
-}
-
-getDate()
-
-
-function getWeekDay(){
-    console.log("date geted with split:\n",date.format("LLLL").split(",")[0])
-}
-
-getWeekDay()
+app.listen(PORT, HOST, () => {
+    console.log(`http://${HOST}:${PORT}`)
+})
