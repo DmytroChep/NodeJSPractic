78c6b86bce6a4d885146927ddcbec119775e34f1
dimachep1408 <dmitriychep2011@gmail.com>
2025-09-25 14:48:55 +0300

    added query params

--- script.js
+++ script.js
@@ -52,11 +52,66 @@ const jsonPath = path.join(__dirname, "posts.json")
 const postsFromJson = JSON.parse(fs.readFileSync(jsonPath, "utf8"))
 
 app.get("/posts", (req, res) => {
-    res.status(418).json(postsFromJson)
+    const filter = Boolean(req.query.filter)
+    console.log(filter)
+    if (filter === true){
+        const filteredPostsFromJson = postsFromJson.map((element) => {
+            if (element["title"].includes("a")){
+                return element
+            }
+        })
+
+        const skipElements = Number(req.query.skip)
+        const getElements = req.query.take 
+
+        if(getElements === undefined){
+            const finalPosts = filteredPostsFromJson.slice(skipElements)
+            res.status(404).json(finalPosts) 
+            return;
+        }
+        const finalgetElements = Number(req.query.take)
+        
+        
+        if(isNaN(skipElements) || isNaN(finalgetElements)){
+            res.status(400).json({status:"take or get must be int!"})
+            return;
+        }
+
+        const finalPosts = filteredPostsFromJson.slice(skipElements, finalgetElements + skipElements)
+        res.status(200).json(finalPosts)
+    } else{
+        const skipElements = Number(req.query.skip)
+        const getElements = req.query.take 
+        
+        if(getElements === undefined){
+            const finalPosts = postsFromJson.slice(skipElements)
+            res.status(404).json(finalPosts) 
+            return;
+        }
+        const finalgetElements = Number(req.query.take)
+        
+        if(isNaN(skipElements) || isNaN(finalgetElements)){
+            res.status(400).json({status:"take or get must be int!"})
+            return;
+        }
+
+        const finalPosts = postsFromJson.slice(skipElements, finalgetElements + skipElements)
+        res.status(200).json(finalPosts)
+    }
+        
+})
+
+app.get("/posts/:id", (req, res) => {
+    const post = postsFromJson[req.params.id]
+    if (post){
+        res.status(200).json({post: postsFromJson[postId]})
+    } else{
+        res.status(404).json({"status": "not found"})
+    }
 })
 
 app.get("/timestamp", (req, res) => {
-    res.status(418).json({date: getFullDate()})
+    res.status(200).json({date: getFullDate()})
 })
 
 app.listen(PORT, HOST, () => {
