18904fbd9e83ce6ad9d7916a407ec2eabc2d68b0
dimachep1408 <dmitriychep2011@gmail.com>
2025-11-11 18:56:33 +0200

    added check of string return |  user controller

--- src/User/user.controller.ts
+++ src/User/user.controller.ts
@@ -7,12 +7,20 @@ export const UserController: ControllerContract = {
         
         const response = await UserService.registration(body)
 
+        if (typeof response === "string"){
+            res.status(400).json(response)
+        }
+
         res.status(200).json(response)
     },
     login: async (req, res) => {
         const body = req.body
         
         const response = await UserService.login(body)
+
+        if (typeof response === "string"){
+            res.status(400).json(response)
+        }
 
         res.status(200).json(response)
     }
